04593c7de2b1e004b94c6df44a3b9b5b9bdbeabc
= <gochujang87@gmail.com>
2025-04-08 23:07:27 +0900

    modify

--- dags/dags_python_with_op_kwargs.py
+++ dags/dags_python_with_op_kwargs.py
@@ -2,7 +2,7 @@ from airflow import DAG
 import pendulum
 import datetime
 from airflow.operators.python import PythonOperator
-from common.common_func import regist
+from common.common_func import regist2
 
 with DAG(
     dag_id="dags_python_with_op_kwargs",
