df2d2060ec9c8751dec8563dd0329fd6c49f5311
= <gochujang87@gmail.com>
2025-04-09 22:15:00 +0900

    template

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2023,3,10,tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+
+    @task(task_id="python_task")
+    def show_templates(**kwargs):
+        print(kwargs)
+
+    show_templates()
